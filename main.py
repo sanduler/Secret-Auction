f28222b574d0e708fdff6b87bc097c3c239f70a2
Sanduler <50250694+sanduler@users.noreply.github.com>
2022-01-20 15:29:41 -0800

    completed for first set of logic from the diagram

--- main.py
+++ main.py
@@ -5,3 +5,13 @@ from ui import logo
 
 # print the logo for the program
 print(logo)
+
+# auction dictionary
+auction = {}
+while True:
+    name = input("What is your name?: ")
+    bid = input("What is your bid?: $")
+    auction[name] = bid
+    again = input("Are there any other bidders? Type 'yes or 'no'.\n").lower()
+    if again == "no":
+        break
